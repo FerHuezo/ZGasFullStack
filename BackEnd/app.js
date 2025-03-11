fd3ea804fb7528d785d65342cf647d200d9a24b8
FerEchoTechs <fernando.ramirez@echotechs.net>
2025-03-11 06:16:20 -0600

    push

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,5 +1,10 @@
 import express from "express";
+import productsRoutes from "./src/routes/productsRoutes.js"
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/products", productsRoutes)
 
 export default app;
